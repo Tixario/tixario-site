ebf02069041d42e6ace03ce61c52be60e57ad47d
Tixario <contact@tixario.com>
2025-05-06 17:26:18 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 export default function Home() {
-  console.log('test redeploy'); // ✅ à l'intérieur de la fonction
-
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold">Hello depuis Tixario 🎟️</h1>
+    <div className="bg-red-500 text-white p-4">
+      Si tu vois cette boîte rouge, Tailwind fonctionne ✅
     </div>
   );
 }
