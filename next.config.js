2ae24b3bf57a4e7b6b4b1c1675f9591e64e2be16
Tixario <contact@tixario.com>
2025-05-06 13:43:07 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  experimental: {
+    appDir: true,
+    runtime: 'nodejs',
+    serverComponents: true
+  },
+  images: {
+    domains: ['your-domain.com'] // Add your domain
+  }
+};
