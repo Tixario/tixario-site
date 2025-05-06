dbf4d2443afa5acc7568ceb416e8dff70223a6df
Tixario <contact@tixario.com>
2025-05-06 17:19:49 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 export default function Home() {
+  console.log('test redeploy'); // ✅ à l'intérieur de la fonction
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
       <h1 className="text-4xl font-bold">Hello depuis Tixario 🎟️</h1>
     </div>
   );
 }
-
-console.log('test redeploy')
-
